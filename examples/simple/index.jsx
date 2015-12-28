169f2db7f6b67928ac978d464786af5acb7160b6
coderkevin <kevin@redfenix.com>
2015-12-28 00:25:52 -0600

    Add webpack and stubs for example code.

---
+++ examples/simple/index.jsx
@@ -0,0 +1,8 @@
+import 'babel-core/polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+  <h1>Test</h1>
+);
+
---
+++ examples/simple/webpack.config.js
@@ -0,0 +1,28 @@
+var path = require( 'path' );
+var webpack = require( 'webpack' );
+
+module.exports = {
+	devtool: 'cheap-module-eval-source-map',
+	entry: [
+		'webpack-hot-middleware/client',
+		'./index.jsx'
+	],
+	output: {
+		path: path.join( __dirname, 'dist' ),
+		filename: 'bundle.js',
+		publicPath: '/static/'
+	},
+	plugins: [
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin()
+	],
+	module: {
+		loaders: [ {
+			test: /\.jsx?$/,
+			loaders: [ 'babel' ],
+			exclude: /node_modules/,
+			include: __dirname
+		} ]
+	}
+};
